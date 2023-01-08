3e580b74bfab0970547115071faef06f80fb7d3b
Marcos Porto <marcosarnoso@msn.com>
2023-01-08 19:40:36 +0100

    Presentación de ejercicios resueltos

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Escribe un programa que reciba tu nombre y lo escriba por consola.
+string? name;
+do {
+    Console.WriteLine("Escribe tu nombre:");
+    name = Console.ReadLine();
+    if(string.IsNullOrEmpty(name)) {
+        Console.WriteLine("Debes especificar un nombre");
+    }
+} while (string.IsNullOrEmpty(name));
+
+Console.WriteLine($"Hola {name}.");
---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,3 @@
+﻿// Escribe un programa que tome la hora y la escriba por pantalla.
+DateTime now = DateTime.Now;
+Console.WriteLine($"Son las {now:HH:mm} horas.");
